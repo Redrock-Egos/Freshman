e32c61d4203243087cf7746abbe755a8b5132727
hosigus <hosigus9733@gmail.com>
2018-08-13 13:53:10 +0800

    mvp fix by acemurder,add photo viewer by hosigus at 2018-8-13

--- app/src/main/java/com/mredrock/cyxbs/freshman/mvp/contract/MainContract.kt
+++ app/src/main/java/com/mredrock/cyxbs/freshman/mvp/contract/MainContract.kt
@@ -1,16 +1,24 @@
 package com.mredrock.cyxbs.freshman.mvp.contract
 
-import android.widget.ImageView
 import com.mredrock.cyxbs.freshman.mvp.presenter.IBasePresenter
 
 class MainContract {
 
     interface IMainView : BaseContract.ISomethingView {
+        /**
+         * @param pos 解锁到的位置
+         * 如 解锁到2.校园攻略
+         * pos=2
+         */
         fun initBuilding(pos: Int)
-        fun getCars(): Array<ImageView>
-        fun getPassCar(pos: Int): ImageView
-        fun getScale(): Float
-        fun unlockBuilding(pos: Int)
+        fun initCars(pos: Int)
+
+        /**
+         * @param pos 目标车位
+         * 如 1.入学必备 -> 2.校园攻略
+         * pos=2
+         */
+        fun driveAnimator(pos: Int)
     }
 
     interface IMainModel {
@@ -19,8 +27,19 @@ class MainContract {
     }
 
     interface IMainPresenter : IBasePresenter<IMainView> {
-        fun onBuildingClick(view: ImageView)
+        fun onBuildingClick(id: Int)
+        /**
+         * 建筑物位置的初始化
+         */
         fun onContentViewSet()
+        /**
+         * 这里要换车
+         */
         fun onResume()
+
+        /**
+         * 回调炼狱
+         */
+        fun onDriveEnd(pos: Int)
     }
 }
